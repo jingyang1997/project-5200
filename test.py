b9aededbc51446f8b4212ef46266346ff5954096
huskyjj <jin.jingy@husky.neu.edu>
2020-10-29 17:52:17 -0400

    test

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 from DataProcessing.DataProcessor import DataProcessor
 
+# 5 here means the minimum length of each sentences
 dataProcessor = DataProcessor(5)
+# X is the [The number of entries * The length of each entry] input list
+# Y is the [The number of entries * 1] label list
 X, Y = dataProcessor.dataProcessing("xss-train.txt", "labeled_data.csv")
